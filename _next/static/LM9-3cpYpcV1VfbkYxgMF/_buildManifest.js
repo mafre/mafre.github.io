d01fdd132ed828a0bac38d8f1a13971e6579981a
Mathias Fredriksson <mathiasfr@gmail.com>
2023-12-22 15:08:24 +0100

    ny

---
+++ _next/static/LM9-3cpYpcV1VfbkYxgMF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-0ffac66cb3fae446.js"],"/jul":["static/css/456eb0e49357a873.css","static/chunks/pages/jul-b3c42a73e6967fb2.js"],sortedPages:["/_app","/_error","/jul"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/breathe.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Breathe {
-    Inhale,
-    Exhale,
-    Hold,
-}
