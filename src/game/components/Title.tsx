84e05d6d630a8e22268c04fb9ab11eddb952cbd1
Mathias Fredriksson <mathiasfr@gmail.com>
2025-11-16 09:51:45 +0100

    build

--- src/game/components/Title.tsx
+++ src/game/components/Title.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import type { OrnamentCSSVars } from '../types/ornament';
 import { Frame } from './Frame';
 
 export interface TitleProps {
@@ -6,12 +7,13 @@ export interface TitleProps {
 }
 
 export function Title({ children }: TitleProps) {
+	let ornamentY = useMemo(() => Math.floor(Math.random() * 4), []);
 	return (
 		<div className="p-2">
 			<Frame
 				mode="dark"
 				body={
-					<div className="modal-header">
+					<div style={{ '--ornament-y': ornamentY } as OrnamentCSSVars} className="modal-header">
 						{children}
 					</div>
 				}
